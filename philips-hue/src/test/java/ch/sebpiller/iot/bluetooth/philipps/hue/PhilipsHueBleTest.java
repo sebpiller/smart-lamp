bd2cf2c64c253d02f275a8205192ac8fb163c783
Seb P <me@sebpiller.ch>
2020-10-07 23:34:20 +0200

    .

--- philips-hue/src/test/java/ch/sebpiller/iot/bluetooth/philipps/hue/PhilipsHueBleTest.java
+++ philips-hue/src/test/java/ch/sebpiller/iot/bluetooth/philipps/hue/PhilipsHueBleTest.java
@@ -2,9 +2,10 @@ package ch.sebpiller.iot.bluetooth.philipps.hue;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
-//@Ignore("ignore unless you have a real Philips Hue bulb connected to your system")
+@Ignore("ignore unless you have a real Philips Hue bulb connected to your system")
 public class PhilipsHueBleTest {
     static String mac = "D7:0E:87:C6:D2:58";
     private PhilipsHueBle test;
