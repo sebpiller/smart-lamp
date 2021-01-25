dd88b60f8ab3443c59f3acec1c774ba1284c0b38
S. Piller <me@sebpiller.ch>
2021-01-25 05:28:54 +0100

    .

--- smart-lamp-amqp-web/src/main/java/ch/sebpiller/iot/config/ServicesConfiguration.java
+++ smart-lamp-amqp-web/src/main/java/ch/sebpiller/iot/config/ServicesConfiguration.java
@@ -2,14 +2,20 @@ package ch.sebpiller.iot.config;
 
 import ch.sebpiller.iot.service.AmqpPublisher;
 import ch.sebpiller.iot.service.impl.AmqpPublisherImpl;
+import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServicesConfiguration {
     @Bean
-    public AmqpPublisher amqpPublisher(ApplicationProperties applicationProperties) {
-        AmqpPublisherImpl amqpPublisher = new AmqpPublisherImpl(applicationProperties.getAmqpConnectionFactory());
+    public AmqpPublisher amqpPublisher(ConnectionFactory cf) {
+        AmqpPublisherImpl amqpPublisher = new AmqpPublisherImpl(cf);
         return amqpPublisher;
+    }
+
+    @Bean
+    public ConnectionFactory getAmqpConnectionFactory(ApplicationProperties applicationProperties) {
+        return applicationProperties.getAmqpConnectionFactory();
     }
 }
