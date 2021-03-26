4baddd6bb6911d1b3f440561ee21fefb84753917
S. Piller <me@sebpiller.ch>
2021-03-26 15:09:41 +0100

    pipeline

---
+++ bluetooth/src/test/java/ch/sebpiller/iot/bluetooth/bluez/BluetoothHelperTest.java
@@ -0,0 +1,12 @@
+package ch.sebpiller.iot.bluetooth.bluez;
+
+import org.junit.jupiter.api.Test;
+
+public class BluetoothHelperTest {
+    @Test
+    public void test() {
+        // dump test
+        BluetoothHelper.reconnectIfNeeded(null);
+    }
+
+}
